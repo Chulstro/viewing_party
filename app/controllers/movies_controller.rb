78c6bfb01be3cf7cd62d05c63afd42c0184a392c
Chandler Hulstrom <chulstro11@gmail.com>
2020-08-25 17:52:02 -0600

    Hotfixes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   def index
     if params[:search].nil? == false
       @movie_details = MovieData.search_results(params[:search])
-    else params[:q].nil? == false
+    else !params[:q].nil?
       @movie_details = MovieData.top_40_movies_by_rating
     end
   end
