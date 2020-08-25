66ee3d675d4e6df8130ce7ef967bb4229a3704e7
Chandler Hulstrom <chulstro11@gmail.com>
2020-08-25 17:29:21 -0600

    Test for movie show page complete

---
+++ spec/features/movies/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'Movies show page' do
+  before :each do
+    visit '/movies'
+    fill_in :search, with: "Avatar"
+    click_on "Find Movies"
+  end
+
+  it "When visiting a page, I should see details of that movie" do
+    within "#0" do
+      click_on "Avatar"
+    end
+    expect(page).to have_content(7.4)
+    expect(page).to have_content("162 minutes")
+    expect(page).to have_content("Action")
+    expect(page).to have_content("Sam Worthington playing as Jake Sully")
+    expect(page).to have_content(2)
+    expect(page).to have_content("daisyrowley")
+    expect(page).to have_content("In the 22nd century")
+  end
+end
