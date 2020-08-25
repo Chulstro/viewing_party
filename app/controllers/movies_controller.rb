f6f8a6dd3572a7e6ee36f8c853e4e5f4bd72e6d8
Chandler Hulstrom <chulstro11@gmail.com>
2020-08-25 17:30:20 -0600

    Completed basic movie show page functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,9 @@ class MoviesController < ApplicationController
     @movie_details = MovieData.search_results(params[:search]) if params[:search].nil? == false
     return @movie_details = MovieData.top_20_rated_movies if params[:q]
   end
+
+  def show
+    movie_info = MovieData.all_movie_att(params[:id])
+    @movie = Movie.new(movie_info)
+  end
 end
